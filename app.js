4ab770aeff9ba969f5a0d80ffbaeeba8d044076c
AgataKondzior <ak.kondzior@gmail.com>
2017-04-21 19:58:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 $(document).ready(function(){
     
     $('#filters').on('click', '.on-sale', function() {
-    $('[data-discount]').toggleClass('highlight-sale');
-  });
+        $('[data-category = "on-sale"]').toggleClass('highlight-sale');
+    });
 
-  $('#filters').on('click', '.featured', function() {
-     $('[data-discount]').toggleClass('highlight-featured');
+    $('#filters').on('click', '.featured', function() {
+        $('[data-category= "featured"]').toggleClass('highlight-featured');
       
-  });
+    });
     
 
     $('button').on('click', function() {
         var discount = $(this).closest('.tour').data('discount');
         var message = $('<div id="message">Call 111-222-333 for a $' + discount + ' discount</div>');
-    $(this).closest('.tour').append(message);
-    $(this).closest('.book').remove();
+       
+        $(this).closest('.tour').append(message);
+        $(this).closest('.book').remove();
       
-  });  
+    });  
     
-})
+});
+      
+
+
+    
       
 
 
